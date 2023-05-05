72795b731b1e21eea4a43044eee42034c384220c
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-05 14:04:29 +0200

    initialized project

---
+++ src/main/java/com/project/videotecha/VideotechaApplication.java
@@ -0,0 +1,13 @@
+package com.project.videotecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotechaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotechaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/videotecha/VideotechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.videotecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
