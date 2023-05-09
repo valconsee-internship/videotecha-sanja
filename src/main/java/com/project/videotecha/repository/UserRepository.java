36d2aba34e7ae6c90f98ec8d497c6a1be036672c
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-09 15:34:01 +0200

    removed unused method

--- src/main/java/com/project/videotecha/repository/UserRepository.java
+++ src/main/java/com/project/videotecha/repository/UserRepository.java
@@ -4,5 +4,4 @@ import com.project.videotecha.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
 }
