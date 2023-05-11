5c94581015fb74c6a5c9c28bc9ef02a37a3d276d
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-11 12:36:40 +0200

    VAL-3 remove unused setters in Theater class

--- src/main/java/com/project/videotecha/model/Theater.java
+++ src/main/java/com/project/videotecha/model/Theater.java
@@ -28,23 +28,11 @@ public class Theater {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getCapacity() {
         return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
     }
 }
