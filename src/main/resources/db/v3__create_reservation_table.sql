de2177acf2071e4339cdeb3289909ee581c2bbb9
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-19 09:22:02 +0200

    VAL-5 refactor sql scripts

--- src/main/resources/db/v3__create_reservation_table.sql
+++ src/main/resources/db/v3__create_reservation_table.sql
@@ -2,9 +2,7 @@ create table reservation (
        id bigserial not null,
         number_of_seats integer,
         canceled boolean,
-        projection_id bigint not null,
-        user_id bigint not null,
+        projection_id bigint not null references projection (id),
+        user_id bigint not null references "user" (id),
         primary key (id)
     );
-alter table if exists reservation add constraint fk_projection_id  foreign key (projection_id) references projection (id);
-alter table if exists reservation add constraint fk_user_id foreign key (user_id) references "user" (id);
