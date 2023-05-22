79d178fde314b0be89973184f66e321c9908449e
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-22 16:26:12 +0200

    VAL-8 add exception handler for DTO validation failure

--- src/main/java/com/project/videotecha/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/project/videotecha/exception/RestResponseEntityExceptionHandler.java
@@ -2,15 +2,17 @@ package com.project.videotecha.exception;
 
 import com.project.videotecha.dto.ExceptionDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
 @RestControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = ApiBadRequestException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
@@ -22,5 +24,15 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ExceptionDto handleNotFoundException(EntityNotFoundException e) {
         return new ExceptionDto(e.getMessage(), Instant.now(), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public String handleMethodValidationFailure(final MethodArgumentNotValidException ex) {
+        StringBuilder b = new StringBuilder();
+        BindingResult results = ex.getBindingResult();
+        for (FieldError e : results.getFieldErrors()) {
+            b.append(e.getDefaultMessage()).append("\n");
+        }
+        return b.toString();
     }
 }
