71b73e2df7f1a37f90a9feb3530f4c4d4c26c573
Todor Popovic <todorpopovic@Todors-MacBook-Pro.local>
2023-05-23 16:07:56 +0200

    VAL-11 Converter for password encryption

--- src/main/java/com/project/videotecha/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/project/videotecha/service/impl/AuthenticationServiceImpl.java
@@ -3,23 +3,18 @@ package com.project.videotecha.service.impl;
 import com.project.videotecha.model.User;
 import com.project.videotecha.service.AuthenticationService;
 import com.project.videotecha.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
 
-    private final PasswordEncoder passwordEncoder;
-
-    public AuthenticationServiceImpl(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthenticationServiceImpl(UserService userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(User newUser) {
-        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         return userService.registerUser(newUser);
     }
 
