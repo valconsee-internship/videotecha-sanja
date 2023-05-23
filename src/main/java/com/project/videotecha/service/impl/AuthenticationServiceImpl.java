5c6c80529cd1eb879a212a981fbe4e66a1719026
Todor Popovic <todorpopovic@Todors-MacBook-Pro.local>
2023-05-23 14:24:40 +0200

    VAL-11 Password encode

--- src/main/java/com/project/videotecha/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/project/videotecha/service/impl/AuthenticationServiceImpl.java
@@ -1,20 +1,26 @@
 package com.project.videotecha.service.impl;
 
 import com.project.videotecha.model.User;
+import com.project.videotecha.model.enums.UserType;
 import com.project.videotecha.service.AuthenticationService;
 import com.project.videotecha.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
 
-    public AuthenticationServiceImpl(UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationServiceImpl(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(User newUser) {
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         return userService.registerUser(newUser);
     }
 
