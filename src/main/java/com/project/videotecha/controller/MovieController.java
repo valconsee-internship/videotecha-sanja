7a709e32d9005a3e0dd8a860f30ff741233425a5
Sanja Drinić <73306238+sanjadrinic@users.noreply.github.com>
2023-05-25 11:07:04 +0200

    VAL-10 remove unnecessary PreAuthorize annotation

--- src/main/java/com/project/videotecha/controller/MovieController.java
+++ src/main/java/com/project/videotecha/controller/MovieController.java
@@ -7,7 +7,6 @@ import com.project.videotecha.mapper.MovieMapper;
 import com.project.videotecha.service.MovieService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,6 @@ public class MovieController implements MovieControllerApi {
         this.movieService = movieService;
     }
 
-    @PreAuthorize("hasAuthority('')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MovieDto create(@Valid @RequestBody MovieCreationDto movieCreationDto) {
